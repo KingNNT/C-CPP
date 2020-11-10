31c68e24528c7f191257a403e7246170a2554987
KingNNT <Dev.KingNNT@gmail.com>
2020-11-10 23:35:46 +0700

    Update

---
+++ Source/Basic/CMake/Ex1/build/CMakeFiles/hellworld.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hellworld.dir/src/greetings_en.cpp.o"
+  "CMakeFiles/hellworld.dir/src/greetings_ja.cpp.o"
+  "CMakeFiles/hellworld.dir/src/greetings_vi.cpp.o"
+  "CMakeFiles/hellworld.dir/src/main.cpp.o"
+  "hellworld"
+  "hellworld.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hellworld.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
